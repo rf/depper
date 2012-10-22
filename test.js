410328e35f672a8eb77a2d38122ca78897f79897
Russ Frank <russ.frank@rutgers.edu>
2012-10-22 17:53:39 -0400

    real tests

--- test.js
+++ test.js
@@ -2,18 +2,39 @@ var assert = require('assert');
 var Depper = require('./depper');
 
 suite('solve', function () {
-  test('solve', function () {
+  test('1', function () {
 
     var set = new Depper();
     set.pkg("foo", "1.3", "1.2", "1.1");
     set.pkg("bar", "1.3", "1.2", "1.1");
     set.pkg("baz", "1.3", "1.2", "1.1");
 
-    set.dep("foo@1.1", "bar@1.2");
+    set.dep("foo@1.3", "bar@1.2");
     set.dep("baz@1.3", "bar@1.3");
     set.dep("baz@1.2", "bar@1.2");
 
     var results = set.solve();
 
+    assert(results.indexOf("foo@1.3") !== -1);
+    assert(results.indexOf("baz@1.2") !== -1);
+    assert(results.indexOf("bar@1.2") !== -1);
+  });
+
+  test('2', function () {
+
+    var set = new Depper();
+    set.pkg("foo", "1.3", "1.2", "1.1");
+    set.pkg("bar", "1.3", "1.2", "1.1");
+    set.pkg("baz", "1.3", "1.2", "1.1");
+
+    set.dep("foo@1.3", "bar@1.2");
+    set.dep("baz@1.3", "bar@1.3");
+    set.dep("baz@1.2", "bar@1.3");
+
+    var results = set.solve();
+
+    assert(results.indexOf("foo@1.3") !== -1);
+    assert(results.indexOf("baz@1.1") !== -1);
+    assert(results.indexOf("bar@1.2") !== -1);
   });
 });
